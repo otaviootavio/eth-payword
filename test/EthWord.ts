5353b2a1f30b2f269937e7fa3357e85de9bd802a
otaviootavio <otaviovacari@usp.br>
2024-04-27 12:13:45 -0300

    add users and hashchain tests

--- test/EthWord.ts
+++ test/EthWord.ts
@@ -96,15 +96,11 @@ describe("EthWord", function () {
 
   describe("Channel", function () {
     it("Should close the channel correctely", async function () {
-      const { ethWord, owner, otherAccount, publicClient, hashChain } =
+      const { ethWord, chainSize, otherAccount, publicClient, hashChain } =
         await loadFixture(deployOneYearLockFixture);
 
-      // await ethWord.write.closeChannel([hashChain[0], 10n], {
-      //   account: otherAccount.account,
-      // });
-
       await ethWord.write.closeChannel(
-        [bytesToHex(hashChain[0], { size: 32 }), 10n],
+        [bytesToHex(hashChain[0], { size: 32 }), BigInt(chainSize)],
         { account: otherAccount.account }
       );
 
@@ -113,8 +109,165 @@ describe("EthWord", function () {
       ).to.equal(0n);
     });
 
-    // TODO
-    // TEST IF THE SEND AMMOUNT IS CORRECT
-    // TEST THE FAIL CASES
+    it("Should send one of n hash", async function () {
+      const {
+        ethWord,
+        chainSize,
+        otherAccount,
+        publicClient,
+        hashChain,
+        ammount,
+      } = await loadFixture(deployOneYearLockFixture);
+
+      await ethWord.write.closeChannel(
+        [bytesToHex(hashChain[chainSize - 1], { size: 32 }), BigInt(1)],
+        { account: otherAccount.account }
+      );
+
+      expect(
+        await publicClient.getBalance({ address: ethWord.address })
+      ).to.equal(ammount - ammount / BigInt(chainSize));
+    });
+
+    it("Should send m of n hash", async function () {
+      const {
+        ethWord,
+        chainSize,
+        otherAccount,
+        publicClient,
+        hashChain,
+        ammount,
+      } = await loadFixture(deployOneYearLockFixture);
+
+      const M = 8;
+
+      await ethWord.write.closeChannel(
+        [bytesToHex(hashChain[chainSize - M], { size: 32 }), BigInt(M)],
+        { account: otherAccount.account }
+      );
+
+      expect(
+        await publicClient.getBalance({ address: ethWord.address })
+      ).to.equal(ammount - (BigInt(M) * ammount) / BigInt(chainSize));
+    });
+  });
+
+  describe("User", function () {
+    it("Should receive the full channel", async function () {
+      const {
+        ethWord,
+        chainSize,
+        otherAccount,
+        publicClient,
+        hashChain,
+        ammount,
+      } = await loadFixture(deployOneYearLockFixture);
+
+      const initialOtherBalance = await publicClient.getBalance({
+        address: otherAccount.account.address,
+      });
+
+      const txResponseId = await ethWord.write.closeChannel(
+        [bytesToHex(hashChain[0], { size: 32 }), BigInt(chainSize)],
+        { account: otherAccount.account }
+      );
+
+      const txReceipt = await publicClient.getTransactionReceipt({
+        hash: txResponseId,
+      });
+
+      const gasUsed = BigInt(txReceipt.gasUsed.toString());
+      const gasPriceUsed = BigInt(txReceipt.effectiveGasPrice);
+
+      const actualFee = gasUsed * gasPriceUsed;
+
+      const finalOtherBalance = await publicClient.getBalance({
+        address: otherAccount.account.address,
+      });
+
+      expect(finalOtherBalance).to.equal(
+        initialOtherBalance + ammount - actualFee
+      );
+    });
+
+    it("Should receive one hash of the channel", async function () {
+      const {
+        ethWord,
+        chainSize,
+        otherAccount,
+        publicClient,
+        hashChain,
+        ammount,
+      } = await loadFixture(deployOneYearLockFixture);
+
+      const initialOtherBalance = await publicClient.getBalance({
+        address: otherAccount.account.address,
+      });
+
+      const txResponseId = await ethWord.write.closeChannel(
+        [bytesToHex(hashChain[chainSize - 1], { size: 32 }), BigInt(1)],
+        { account: otherAccount.account }
+      );
+
+      const txReceipt = await publicClient.getTransactionReceipt({
+        hash: txResponseId,
+      });
+
+      const gasUsed = BigInt(txReceipt.gasUsed.toString());
+      const gasPriceUsed = BigInt(txReceipt.effectiveGasPrice);
+
+      const actualFee = gasUsed * gasPriceUsed;
+
+      const finalOtherBalance = await publicClient.getBalance({
+        address: otherAccount.account.address,
+      });
+
+      const ammountOneHash = ammount / BigInt(chainSize);
+
+      expect(finalOtherBalance).to.equal(
+        initialOtherBalance + ammountOneHash - actualFee
+      );
+    });
+
+    it("Should receive M hash of the channel", async function () {
+      const {
+        ethWord,
+        chainSize,
+        otherAccount,
+        publicClient,
+        hashChain,
+        ammount,
+      } = await loadFixture(deployOneYearLockFixture);
+
+      const M = 4;
+
+      const initialOtherBalance = await publicClient.getBalance({
+        address: otherAccount.account.address,
+      });
+
+      const txResponseId = await ethWord.write.closeChannel(
+        [bytesToHex(hashChain[chainSize - M], { size: 32 }), BigInt(M)],
+        { account: otherAccount.account }
+      );
+
+      const txReceipt = await publicClient.getTransactionReceipt({
+        hash: txResponseId,
+      });
+
+      const gasUsed = BigInt(txReceipt.gasUsed.toString());
+      const gasPriceUsed = BigInt(txReceipt.effectiveGasPrice);
+
+      const actualFee = gasUsed * gasPriceUsed;
+
+      const finalOtherBalance = await publicClient.getBalance({
+        address: otherAccount.account.address,
+      });
+
+      const ammountToMHashes = (BigInt(M) * ammount) / BigInt(chainSize);
+
+      expect(finalOtherBalance).to.equal(
+        initialOtherBalance + ammountToMHashes - actualFee
+      );
+    });
   });
 });
