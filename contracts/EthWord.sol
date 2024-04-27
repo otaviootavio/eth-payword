f6e3b0eff94e0812c7e0d5174a4c608850559778
otaviootavio <otaviovacari@usp.br>
2024-04-27 14:11:25 -0300

    add dry run

--- contracts/EthWord.sol
+++ contracts/EthWord.sol
@@ -28,19 +28,47 @@ contract EthWord {
             msg.sender == channelRecipient,
             "Only the recipient can close the channel"
         );
-        bytes32 wordScratch = _word;
-        for (uint i = 1; i < _wordCount; i++) {
-            wordScratch = keccak256(abi.encodePacked(wordScratch));
-        }
-        require(wordScratch == channelTip, "Incorrect word or word count");
+        validateChannelClosure(_word, _wordCount);
 
         uint amountToWithdraw = calculateWithdrawAmount(_wordCount);
 
         (bool sent, ) = channelRecipient.call{value: amountToWithdraw}("");
         require(sent, "Failed to send Ether");
         // TODO
-        // transfer the remaining ammount to sender
+        // transfer the remaining amount to sender
         // channelSender
+    }
+
+    function simulateCloseChannel(
+        bytes32 _word,
+        uint _wordCount
+    ) public view returns (bool, uint) {
+        require(
+            msg.sender == channelRecipient,
+            "Only the recipient can simulate closing the channel"
+        );
+        if (isChannelClosed) {
+            return (false, 0);
+        }
+
+        bool isValid = validateChannelClosure(_word, _wordCount);
+        if (!isValid) {
+            return (false, 0);
+        }
+
+        uint amountToWithdraw = calculateWithdrawAmount(_wordCount);
+        return (true, amountToWithdraw);
+    }
+
+    function validateChannelClosure(
+        bytes32 _word,
+        uint _wordCount
+    ) private view returns (bool) {
+        bytes32 wordScratch = _word;
+        for (uint i = 1; i < _wordCount; i++) {
+            wordScratch = keccak256(abi.encodePacked(wordScratch));
+        }
+        return wordScratch == channelTip;
     }
 
     function calculateWithdrawAmount(
